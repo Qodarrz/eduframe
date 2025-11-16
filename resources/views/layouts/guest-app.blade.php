e0275fba2a0300571718786b0ef5c607b6aadd38
Qodarrz <heidararizki@gmail.com>
2025-11-16 18:00:35 +0700

    fix

--- resources/views/layouts/guest-app.blade.php
+++ resources/views/layouts/guest-app.blade.php
@@ -215,14 +215,6 @@
                                 <span class="group-hover:translate-x-1 transition">School Gallery</span>
                             </a>
                         </li>
-                        <li>
-                            <a href="{{ route('agenda') }}" class="flex items-center space-x-2 text-gray-400 hover:text-white transition group">
-                                <svg class="w-4 h-4" style="color: #6155F5;" fill="currentColor" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path>
-                                </svg>
-                                <span class="group-hover:translate-x-1 transition">Agenda</span>
-                            </a>
-                        </li>
                     </ul>
                 </div>
 
